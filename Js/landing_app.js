2bd6205426a43aa3c293d0abbe499011fa971a9b
Rama2615 <naufalrama49@gmail.com>
2026-07-26 09:36:27 +0700

    Fix bug render kartu UMKM dan implementasi fitur pagination

--- Js/landing_app.js
+++ Js/landing_app.js
@@ -1,9 +1,9 @@
-import { initTheme } from './theme.js?v=8';
-import { UmkmService } from './Services/umkm_services.js?v=8';
-import { UmkmCard } from './components/umkm_card.js?v=8';
-import { initHero3DCanvas } from './components/hero_3d_canvas.js?v=8';
-import { init3DTiltEngine } from './utils/tilt_3d.js?v=8';
-import { init3DMarqueeWall } from './components/marquee_3d.js?v=8';
+import { initTheme } from './theme.js?v=9';
+import { UmkmService } from './Services/umkm_services.js?v=9';
+import { UmkmCard } from './components/umkm_card.js?v=9';
+import { initHero3DCanvas } from './components/hero_3d_canvas.js?v=9';
+import { init3DTiltEngine } from './utils/tilt_3d.js?v=9';
+import { init3DMarqueeWall } from './components/marquee_3d.js?v=9';
 
 // Inisialisasi tema saat halaman dimuat
 initTheme();
